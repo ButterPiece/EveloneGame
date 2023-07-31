5bdc8487b2540bd221efa6d427765e1c13978a8d
zak10110 <35228898+zak10110@users.noreply.github.com>
2023-07-31 19:02:32 +0300

    Cam Rotation Script

--- EveloneGame/Assets/Scripts/CameraRotationStart.cs
+++ EveloneGame/Assets/Scripts/CameraRotationStart.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraRotationStart : MonoBehaviour
 {
+    private float x;
+    private float y;
+    public float yRotation;
+    public float xRotation;
+    public float sensitivity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState=CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x=Input.GetAxis("Mouse X")*Time.deltaTime*sensitivity;
+        y=Input.GetAxis("Mouse Y")*Time.deltaTime*sensitivity;
+        yRotation+=x;
+        xRotation-=y;
+        xRotation=Mathf.Clamp(xRotation,-20f,30f);
+        yRotation=Mathf.Clamp(xRotation,100f,300f);
+        transform.rotation=Quaternion.Euler(xRotation,yRotation,0);
+
     }
 }
